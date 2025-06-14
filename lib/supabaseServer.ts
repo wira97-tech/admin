5dfb850f6ed297b32b137d9be106a011553d4227
algosbiz <contact@algoseabiz.com>
2025-06-15 02:33:56 +0800

    update

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,7 +1,7 @@
-// lib/supabaseAdmin.ts
-import { createClient } from '@supabase/supabase-js'
+    // lib/supabaseAdmin.ts
+    import { createClient } from '@supabase/supabase-js'
 
-export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-)
+    export const supabaseAdmin = createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+    )
